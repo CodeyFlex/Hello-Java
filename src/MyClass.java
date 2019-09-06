75a5b44d996d6b7d8fa6b0fc4df55fb534013692
CodeyFlex <sej920@hotmail.com>
2019-09-06 13:34:39 +0200

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,32 @@
+class MyClass {
+  public static void main(String[ ] args) {
+
+    System.out.println("Hey world, i like water.");
+  //}
+//} This has been commented so i can continue the function further down.
+
+//hello there, this is a single line comment.
+
+/* this on the other hand
+is a multi line comment
+that i will use to tell you my entire life story
+here goes...
+ */
+
+/** This is my documentation comment, which exists to tell you how good a coder i am, and also click the button to start.*/
+
+// Below is an initial test, removed cause of the later test.
+// String name = "Sebastian"
+
+//class MyClass {
+  //public static void main(String[ ] args) {
+    String name ="Sebastian";
+    int age = 26;
+    double score =658.9;
+    char group = 'H';
+    System.out.println(name + " is " + age + " years old & has score: " + score + " in group " + group );
+  }
+}
+
+//Boolean online = True
+//Above boolean doesn't work right now, but is an example of a true/false function, which is what a Boolean is.
