f1d202270c11e82cf6b72eafe9382741b5e9119b
CodeyFlex <Sej920@hotmail.com>
2019-10-08 17:48:01 +0200

    Calculated sum of an array with a for loop

--- src/Sololearn/Arrays.java
+++ src/Sololearn/Arrays.java
@@ -6,28 +6,40 @@ import java.util.ArrayList;
 public class Arrays {
     public static void main(String[] args) {
 
-                    //Arrays starts counting from 0, so an index of 4 boxes would be: 0 1 2 3
-                    String[] shopList = new String[4]; //Here we declare an array of 4 integers
-                    //Lets use these indexes to make a shopping list
-                    shopList[0] = "Salad"; //Index 0
-                    shopList[1] = "Milkshake"; //Index 1
-                    shopList[2] = "Milk"; //Index 2
-                    shopList[3] = "Cat"; //Index 3
+        //Arrays starts counting from 0, so an index of 4 boxes would be: 0 1 2 3
+        String[] shopList = new String[4]; //Here we declare an array of 4 integers
+        //Lets use these indexes to make a shopping list
+        shopList[0] = "Salad"; //Index 0
+        shopList[1] = "Milkshake"; //Index 1
+        shopList[2] = "Milk"; //Index 2
+        shopList[3] = "Cat"; //Index 3
 
-                    System.out.println("Let's print out a shopping list using 4 indexes in an array:");
-                    System.out.println(shopList[0]); //Printing Index 0
-                    System.out.println(shopList[1]); //Printing Index 1
-                    System.out.println(shopList[2]); //Printing Index 2
-                    System.out.println(shopList[3]); //Printing Index 3
+        System.out.println("Let's print out a shopping list using 4 indexes in an array:");
+        System.out.println(shopList[0]); //Printing Index 0
+        System.out.println(shopList[1]); //Printing Index 1
+        System.out.println(shopList[2]); //Printing Index 2
+        System.out.println(shopList[3]); //Printing Index 3
 
-            System.out.println(" "); //Space for fanciness
-            System.out.println("Let's print the array with a loop instead");
-            for (int sL=0; sL<=3; sL++){
-                    System.out.println(shopList[sL]);
-            } //We close the loop with this, otherwise it would loop the following as well: (i could probably also write break)
-                System.out.println(" "); //Space for fanciness
-                System.out.println("Now let's use length to figure out the length of our array:");
-                System.out.println(shopList.length);
+        System.out.println(" "); //Space for fanciness
+        System.out.println("Let's print the array with a loop instead");
+        for (int sL = 0; sL <= 3; sL++) {
+            System.out.println(shopList[sL]);
+        } //We close the loop with this, otherwise it would loop the following as well: (i could probably also write break)
+        System.out.println(" "); //Space for fanciness
 
+        System.out.println("Now let's use length to figure out the length of our array:");
+        System.out.println(shopList.length);
+
+        System.out.println(" "); //Space for fanciness
+        System.out.println("Now let's calculate the sum of all the wares in our shopping list within my array by using a for loop");
+        int[] shopListPrices = {6, 10, 2, 500};
+        System.out.println("The Salad is " + shopListPrices[0] + "$, Milkshake is " + shopListPrices[1] + "$, Milk is " + shopListPrices[2] + "$, and the cat is " + shopListPrices[3] + "$, which all equals to: ");
+        int sum = 0;
+        for (int x = 0; x < shopListPrices.length; x++) {
+            sum += shopListPrices[x];
+        }
+        System.out.println(sum + "$");
+
+        System.out.println(" "); //Space for fanciness
     }
 }
