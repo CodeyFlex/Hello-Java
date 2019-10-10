50e8b6ce5b5061e1685068088ad54ee7a3faddcf
CodeyFlex <Sej920@hotmail.com>
2019-10-10 14:25:02 +0200

    Created my own method

---
+++ src/Sololearn/Classes_and_Objects.java
@@ -0,0 +1,28 @@
+package Sololearn;
+
+public class Classes_and_Objects {
+
+    static void sayCheese() {
+        System.out.println("Cheese!");
+    }
+
+    public static void main(String[] args) {
+
+        //in object oriented programming, each object has three dimensions: identity, attributes, and behavior.
+        // Attributes describe the object's current state, and what the object is capable of doing is demonstrated through the object's behavior.
+
+        //A class describes what the object will be, but is separate from the object itself.
+        //An object is an instance of a class
+
+        //example: Class: Human being       Object : Man, Woman
+
+        //Methods define behavior. System.out.println() is an example of a method.
+
+        System.out.println("I can define my own method as such:");
+        //Type static void sayCheese() above psvm
+        //System.out.println("Cheese!");
+        //}
+        System.out.println("And have it say Cheese!");
+        sayCheese();
+    }
+}
