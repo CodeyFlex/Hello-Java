724bf25b68df4c5f379973d4f11d0e24428aa515
CodeyFlex <Sej920@hotmail.com>
2019-11-10 22:47:38 +0100

    Learned about labels, VBox, ImageView & scenes

--- src/JavaFX_Tutorial/ApplicationONE.java
+++ src/JavaFX_Tutorial/ApplicationONE.java
@@ -1,6 +1,13 @@
 package JavaFX_Tutorial;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,7 +22,34 @@ public class ApplicationONE extends Application {
 
     //the start method is abstract and MUST be overided when making a new javafx app
     @Override
-    public void start(Stage prisonerStage1) throws Exception { //Stage is a window in the GUI
+    public void start(Stage mainStage) throws Exception { //Stage is a window in the GUI
+
+        mainStage.setTitle("Flexy Application");
+        mainStage.setWidth(600);
+        mainStage.setHeight(600);
+        mainStage.toFront(); //Puts this window in front of the others.
+
+        VBox parent = new VBox(); //Parent/root node, stands for vertical box as it displays labels vertically
+
+        ImageView image1 = new ImageView("https://www.idenyt.dk/globalassets/denmark/kaledyr-2/corgi/corgi_jumping.jpg"); //Inserts image into scene from a url
+        parent.getChildren().add(image1);
+
+        Label label1 = new Label("I'm a Corgi!"); //child node to parent node
+        label1.setTextFill(Color.web("#34eb3a")); //Changes color of label
+        label1.setFont(new Font("Times New Roman", 51)); //Changes the font of the label, and the number changes the size (pixel amount)
+        Label label2 = new Label("Where am i going?"); //Label adds text to the stage/scene
+        label2.setRotate(22); //Rotates the label
+        parent.getChildren().add(label1);
+        parent.getChildren().add(label2); //Adds the label to the parent node (VBox), can also do .addAll(label1, label2); so on.
+
+        Scene scene1 = new Scene(parent); //My scene that everything will go under
+        scene1.setCursor(Cursor.CROSSHAIR); //Changes cursor to a crosshair when using the scene
+
+        mainStage.setScene(scene1);
+
+        mainStage.show();
+
+        Stage prisonerStage1 = new Stage();
         prisonerStage1.setTitle("Prisoner 1"); //Giving my stage a name
         prisonerStage1.setWidth(300); //Setting width of stage
         prisonerStage1.setHeight(200); //Setting height of stage
@@ -41,7 +75,7 @@ public class ApplicationONE extends Application {
         captorWindow.setWidth(300);
         captorWindow.setHeight(200);
         captorWindow.setTitle("I am the captor of all these windows, mwahaha!");
-        captorWindow.toFront(); //Puts this window in front of the others.
+        // delete later. captorWindow.toFront(); //Puts this window in front of the others.
         //There's also .setAlwaysOnTop(true); and .setFullScreen(true);
         captorWindow.initModality(Modality.APPLICATION_MODAL); //Other windows can't be used until this one is closed, like if you go to settings in most applications
         //(Modality.NONE); is the default value, and allows for interaction with any window
