7f80b6a6bea0e4a75966c025f2ed120a70b6b3b8
CodeyFlex <Sej920@hotmail.com>
2019-11-14 22:25:04 +0100

    Learned about CSS Styling

--- src/JavaFX_Tutorial/ApplicationONE.java
+++ src/JavaFX_Tutorial/ApplicationONE.java
@@ -1,11 +1,8 @@
 package JavaFX_Tutorial;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -15,7 +12,6 @@ import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import org.w3c.dom.ls.LSOutput;
 
 public class ApplicationONE extends Application {
 
@@ -31,23 +27,33 @@ public class ApplicationONE extends Application {
 
         mainStage.setTitle("Flexy Application");
         mainStage.setWidth(600);
-        mainStage.setHeight(600);
+        mainStage.setHeight(800);
 
-        VBox parent = new VBox(); //Parent/root node, stands for vertical box as it displays labels vertically
+        VBox root = new VBox(); //Parent/root node, stands for vertical box as it displays labels vertically
+        //I can also add stylesheets to my VBox
+        //root.getStylesheets().add("path");
+        //As long as the VBox has child nodes
 
         ImageView image1 = new ImageView("https://www.idenyt.dk/globalassets/denmark/kaledyr-2/corgi/corgi_jumping.jpg"); //Inserts image into scene from a url
-        parent.getChildren().add(image1);
+        root.getChildren().add(image1);
 
         Label label1 = new Label("I'm a Corgi!"); //child node to parent node
         label1.setTextFill(Color.web("#34eb3a")); //Changes color of label
         label1.setFont(new Font("Times New Roman", 51)); //Changes the font of the label, and the number changes the size (pixel amount)
+
         Label label2 = new Label("Where am i going?"); //Label adds text to the stage/scene
         label2.setRotate(22); //Rotates the label
-        parent.getChildren().add(label1);
-        parent.getChildren().add(label2); //Adds the label to the parent node (VBox), can also do .addAll(label1, label2); so on.
+
+        Label label3 = new Label("I am uniqueee");
+        label3.setId("unique-label"); //unique id so i can call this in my stylesheets
+
+        root.getChildren().add(label1);
+        root.getChildren().add(label2); //Adds the label to the parent node (VBox), can also do .addAll(label1, label2); so on.
+        root.getChildren().add(label3);
 
         Hyperlink link = new Hyperlink("Button that prints"); //Creates a hyperlink
-        parent.getChildren().add(link); //gets the hyperlink
+        link.setStyle("-fx-background-color: #ffc67b"); //setStyle directly to this particular hyperlink
+        root.getChildren().add(link); //gets the hyperlink
         link.setOnAction(e -> { //link name: "link" referenced at start, and now i will decide the action when the link is clicked on
             System.out.println("Button was pressed"); //Action when link is clicked on
         });
@@ -59,7 +65,8 @@ public class ApplicationONE extends Application {
        //     }
        // });
 
-        Scene scene1 = new Scene(parent); //My scene that everything will go under
+        Scene scene1 = new Scene(root); //My scene that everything will go under
+        scene1.getStylesheets().add("JavaFX_Tutorial/stylesheets/styles.css"); //Gets styles from my css file. (this apparently doesn't work if i name my VBox parent?)
         scene1.setCursor(Cursor.CROSSHAIR); //Changes cursor to a crosshair when using the scene
 
         mainStage.setScene(scene1);
