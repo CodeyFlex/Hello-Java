d3cff0200ce6ae7f7387cba6f6b040c630686f16
CodeyFlex <Sej920@hotmail.com>
2019-11-19 13:23:06 +0100

    Learned about StackPanes

--- src/JavaFX_Tutorial/StackPane_Application.java
+++ src/JavaFX_Tutorial/StackPane_Application.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class StackPane_Application extends Application {
@@ -21,9 +22,18 @@ public class StackPane_Application extends Application {
         ImageView image = new ImageView("https://www.idenyt.dk/globalassets/denmark/kaledyr-2/corgi/corgi_jumping.jpg");
         Label l1 = new Label("Hi");
         Button b2 = new Button("Hi, click me!");
-        StackPane sPRoot = new StackPane(image, b2, l1); //Child nodes are stacked upon each other in a Stack Pane
+        Label l2 = new Label("He's sitting on meee");
+        Label l3 = new Label("No i'm not. <.<");
+        StackPane sPRoot = new StackPane(image, b2, l1, l2, l3); //Child nodes are stacked upon each other in a Stack Pane
 
         StackPane.setAlignment(b2, Pos.CENTER_LEFT); //Places the b2 node away so it's more visible
+        StackPane.setAlignment(l2, Pos.TOP_CENTER);
+        StackPane.setAlignment(l3, Pos.TOP_CENTER);
+
+        l2.setTextFill(Color.web("#34eb3a"));
+        l3.setTextFill(Color.web("#ffc67b"));
+        l2.setStyle("-fx-font-size: 25px");
+        l3.setStyle("-fx-font-size: 35px");
 
         Scene scene = new Scene(sPRoot);
         stage.setScene(scene);
