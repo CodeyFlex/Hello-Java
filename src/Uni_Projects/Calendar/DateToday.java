8ed21dafa4fb47cb347401c1d7e4974c297f380b
CodeyFlex <Sej920@hotmail.com>
2019-11-23 14:02:29 +0100

    Made a Date retriever and a Random lottery/Seed lottery

--- src/Uni_Projects/Calendar/DateToday.java
+++ src/Uni_Projects/Calendar/DateToday.java
@@ -5,14 +5,14 @@ import java.util.Date;
 public class DateToday {
     public static void main(String[] args) {
         Date date = new Date();
-        Date birthDate = new Date();
+        Date wickDate = new Date();
 
-        birthDate.setTime(734133600000L); //L cause it's a Long number
+        wickDate.setTime(1414101600000L); //L cause it's a Long number
 
         System.out.println("The amount of time since Jan. 1, 1970 is " + date.getTime() + " Milliseconds");
         System.out.println("Todays date is: " + date.toString());
         System.out.println("Todays time is: " + date.getTime());
-        System.out.println("The time since my birth, since making this app is: " + birthDate.getTime() + " Milliseconds");
-        System.out.println("My Birthdate: " + birthDate.toString());
+
+        System.out.println("Date of top tier action movie release: " + wickDate.toString());
     }
 }
